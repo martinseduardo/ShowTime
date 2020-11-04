825c446adcf4499f45b702214bb3b571f3a785ed
unknown <eduardo.martins.go@gmail.com>
2020-11-03 22:55:39 -0300

    Fix service issues

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,3 +1,4 @@
+using Application.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,7 @@ namespace Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICacheService, CacheService>();
             services.AddSwaggerGen();
             services.AddDistributedRedisCache(options =>
             {
